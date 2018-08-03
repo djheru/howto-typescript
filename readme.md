7ea0cd4ed7d9e9a1c078aef43ad589524ad4d3f7
Philip Damra <philip.damra@pdamra.local>
2018-08-03 14:59:10 -0700

    Basic app setup

--- readme.md
+++ readme.md
@@ -6,7 +6,17 @@
 
 ### Using TypeScript
 
+`npm init -y`
+
+`npm i -D lite-server`
+
+`tsc --init`
+
 `touch index.html script.ts`
 
-- Compile the script files
-`tsc script.ts`
+`tsc --init` - Generate tsconfig.json
+
+`tsc` - Find all tsc files and compile em
+
+
+### Using Types for Better Code
--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-function Greeter(greeting) {
-    this.greeting = greeting;
-}
-Greeter.prototype.greet = function () {
-    return "Hello, " + this.greeting;
-};
-var greeter = new Greeter("Ohai!");
-var button = document.createElement("button");
-button.textContent = "say ohai";
-button.onclick = function () {
-    alert(greeter.greet());
-};
-document.body.appendChild(button);
+"use strict";
+console.log('ohai');
